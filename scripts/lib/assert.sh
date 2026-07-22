ad076219a42a1f42d08e896200aaa5442de8560e
Kamilou52 <elkemal2000@yahoo.fr>
2026-07-22 12:30:14 +0200

    End of assertion library V1.1

--- scripts/lib/assert.sh
+++ scripts/lib/assert.sh
@@ -8,12 +8,60 @@ assert_equals() {
 
     local expected="$1"
     local actual="$2"
+    local message="${3:-}"
 
     if [ "$expected" = "$actual" ]
     then
+        if [ -n "$message" ]
+        then
+            print_ok "$message"
+        fi
+
         return 0
     fi
 
+    if [ -n "$message" ]
+    then
+        print_error "$message"
+        echo "Expected : $expected"
+        echo "Actual   : $actual"
+    fi
+
     return 1
+
+}
+
+assert_file_exists() {
+
+    local file="$1"
+
+    [ -f "$file" ]
+
+}
+
+assert_directory_exists() {
+
+    local directory="$1"
+
+    [ -d "$directory" ]
+
+}
+
+assert_command_success() {
+
+    "$@"
+    assert_command_success bash -n scripts/new-project.sh
+    assert_command_success git --version
+
+}
+
+assert_command_failure() {
+
+    if "$@"
+    then
+        return 1
+    fi
+
+    return 0
 
 }
